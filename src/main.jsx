7c95de2610565c3f6bc357ba858914698732a3f9
ff08435 <faisalfatima248@gmail.com>
2026-01-17 15:14:17 +0500

    Enable PWA offline support

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { UserProvider } from "./context/UserContext";
+import { registerSW } from "virtual:pwa-register";
+registerSW();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
