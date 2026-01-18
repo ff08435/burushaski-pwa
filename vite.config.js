07773c93ec5efc25f74b135dac28a85c8f69a7f2
ff08435 <faisalfatima248@gmail.com>
2026-01-18 20:06:19 +0500

    TRIED PUSH NOTIF AGAINA again

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,37 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-VitePWA({
-  registerType: "autoUpdate",
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
 
-  strategies: "injectManifest",
-  srcDir: "src",
-  filename: "sw.js",
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
 
-  manifest: {
-    name: "Burushaski Audio Collection",
-    short_name: "Burushaski",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#0f172a",
-    theme_color: "#facc15",
-    icons: [
-      {
-        src: "/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
+      manifest: {
+        name: "Burushaski Audio Collection",
+        short_name: "Burushaski",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#0f172a",
+        theme_color: "#facc15",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-      {
-        src: "/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
+    }),
+  ],
 });
